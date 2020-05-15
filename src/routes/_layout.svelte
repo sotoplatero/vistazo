57172bf7c34bd04b1c09ed21503542ae9e7b9efc
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-05-15 17:05:19 -0400

    incluyendo el form

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -11,17 +11,42 @@
 	</div>
 </main>
 
-<footer class="py-12 bg-gray-600 text-gray-200 text-center mt-10">
+<footer class="py-12 bg-gray-600 mt-10">
 	<div class="container">
 		<div class="w-full md:w-1/2 mx-auto">
-			<ul>
-		        <li>
-		          <a href="about" class="text-gray-200">Nosotros</a>
-		        </li>		
-		        <li>
-		          <a href="https://github.com/sotoplatero/vistazo.git" target="_blank" class="text-gray-200">Código</a>
-		        </li>		
-			</ul>	
+
+			<p class="mb-6 text-gray-200 text-center">Si quieres incluir alguna información o contactarnos escribenos</p>
+			<form name="contact0" method="POST" action="/success" netlify-honeypot="bot-field" data-netlify="true">
+				<p class="hidden">
+				  <label><input name="bot-field" /></label>
+				</p>
+
+				<div class="mb-4">
+				  <label class="block text-gray-200 text-sm font-bold mb-2">Correo Electrónico</label>
+				  <input class="bg-gray-400 appearance-none border-0 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-gray-200" type="email" name="email">
+				</div>
+
+				<div class="mb-4">
+				  <label class="block text-gray-200 text-sm font-bold mb-2">Mensaje</label>
+				  <textarea
+				    class="bg-gray-400 appearance-none border-0 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-gray-200" name="message" rows="5"></textarea>
+				</div>
+
+				<div class="control">
+				  <button class="bg-gray-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Enviar &rarr;</button>
+				</div>
+			</form>			
+
+          <div class="mt-8">
+			<ul class="">
+			    <li>
+			      <a href="about" class="text-gray-200">Nosotros</a>
+			    </li>		
+			    <li>
+			      <a href="https://github.com/sotoplatero/vistazo.git" target="_blank" class="text-gray-200">Código</a>
+			    </li>		
+			</ul>          
+          </div>
 		</div>
 	</div>
 </footer>
