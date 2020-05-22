d6cf68f720149a3a1b36341babe9fa38f06b8a42
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-05-22 08:12:45 -0400

    update

--- src/components/Podcast.svelte
+++ src/components/Podcast.svelte
@@ -28,10 +28,6 @@
 	<Podcast url={podcast.feed} enclosure="{true}">
 		<footer class="mt-6" slot="footer">
 			<ButtonReload on:click={loadPodcast}/>			
-<!-- 			<Button type="gray" on:click={loadPodcast}>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-cw h-4 w-4 mr-2"><polyline points="23 4 23 10 17"></polyline><polyline points="1 20 1 14 7 14"></polyline><path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path></svg>				
-				<span>Aleatorio</span>
-			</Button> -->
 		</footer>
 	</Podcast>
 {/if}
